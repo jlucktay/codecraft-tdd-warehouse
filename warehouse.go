369421fbc0f56a1e55155f263bff86a0c526117c
James Lucktaylor <jlucktay+github@gmail.com>
2022-05-16 14:24:51 +0100

    feat(warehouse): successfully sell a single CD (without payment)

--- warehouse.go
+++ warehouse.go
@@ -1,3 +1,51 @@
 package warehouse
 
-type CD struct{}
+//   - includes GUID, price, title, artist, label, stock level
+
+type cdUID string
+
+type CD struct {
+	title, artist, label string
+	price                float64
+	stock                uint
+}
+
+type Warehouse struct {
+	stock map[cdUID]*CD
+}
+
+func New() *Warehouse {
+	return &Warehouse{
+		stock: make(map[cdUID]*CD, 0),
+	}
+}
+
+func (w *Warehouse) Add(cd CD) error {
+	return nil
+}
+
+func (w *Warehouse) NewCD(title, artist, label string, price float64, stock uint) cdUID {
+	newID := newCDUID()
+
+	w.stock[newID] = &CD{
+		title:  title,
+		artist: artist,
+		label:  label,
+		price:  price,
+		stock:  stock,
+	}
+
+	return newID
+}
+
+func (w *Warehouse) QueryStockLevel(id cdUID) uint {
+	return w.stock[id].stock
+}
+
+func (w *Warehouse) SellCD(id cdUID) {
+	w.stock[id].stock--
+}
+
+func newCDUID() cdUID {
+	return "asdf"
+}
--- warehouse_test.go
+++ warehouse_test.go
@@ -4,11 +4,23 @@ import (
 	"testing"
 
 	"github.com/matryer/is"
+
+	warehouse "go.jlucktay.dev/tdd-cd-warehouse"
 )
 
 // - sell a single CD to a customer and take payment
 func TestSellSingleCD(t *testing.T) {
 	is := is.New(t)
 
-	is.True(true)
+	// Arrange
+	whse := warehouse.New()
+	newID := whse.NewCD("S&M", "Metallica", "Vertigo Records", 14.99, 3)
+	startingCount := whse.QueryStockLevel(newID)
+
+	// Act
+	whse.SellCD(newID)
+
+	// Assert
+	finalCount := whse.QueryStockLevel(newID)
+	is.True(finalCount == startingCount-1)
 }
