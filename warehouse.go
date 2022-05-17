6f450e73423b217bd1d8cefd483809b57af08ba1
James Lucktaylor <jlucktay+github@gmail.com>
2022-05-17 09:13:41 +0100

    test(warehouse): finish TestLookUpCDByTitle

--- warehouse.go
+++ warehouse.go
@@ -32,25 +32,6 @@ var (
 	ErrCDAlreadyInWarehouse = errors.New("a CD with this ID is already in the warehouse")
 )
 
-func New() *Warehouse {
-	return &Warehouse{
-		p:     &dummyPayments{},
-		stock: make(map[cdUID]*CD, 0),
-	}
-}
-
-func (w *Warehouse) Add(cd CD) (cdUID, error) {
-	newID := newCDUID(cd)
-
-	if _, exists := w.stock[newID]; exists {
-		return "", ErrCDAlreadyInWarehouse
-	}
-
-	w.stock[newID] = &cd
-
-	return newID, nil
-}
-
 func NewCD(title, label string, price float64, stock uint) CD {
 	return CD{
 		title: title,
@@ -62,8 +43,35 @@ func NewCD(title, label string, price float64, stock uint) CD {
 	}
 }
 
-func (w *Warehouse) NewStockedItem(price float64, amount uint) stockedItem {
-	return stockedItem{price: price, stock: amount}
+func (c *CD) GetTitle() string {
+	return c.title
+}
+
+func (c *CD) GetPrice() float64 {
+	return c.i.price
+}
+
+func New() *Warehouse {
+	return &Warehouse{
+		p:     &dummyPayments{},
+		stock: make(map[cdUID]*CD, 0),
+	}
+}
+
+func generateCDUID(cd CD) cdUID {
+	return cdUID(cd.label + "|" + cd.title)
+}
+
+func (w *Warehouse) Add(cd CD) (cdUID, error) {
+	newID := generateCDUID(cd)
+
+	if _, exists := w.stock[newID]; exists {
+		return "", ErrCDAlreadyInWarehouse
+	}
+
+	w.stock[newID] = &cd
+
+	return newID, nil
 }
 
 func (w *Warehouse) BatchReceipt(label string, wr ...WarehouseReceipt) error {
@@ -102,10 +110,12 @@ func (w *Warehouse) SellSingleCD(id cdUID) error {
 	return w.SellCD(id, 1)
 }
 
-func newCDUID(cd CD) cdUID {
-	return cdUID(cd.label + "|" + cd.title)
-}
-
 func (w *Warehouse) FindCDByTitle(title string) (CD, error) {
-	return CD{}, nil
+	for _, cd := range w.stock {
+		if cd.title == title {
+			return *cd, nil
+		}
+	}
+
+	return CD{}, ErrCDNotFound
 }
--- warehouse_test.go
+++ warehouse_test.go
@@ -68,3 +68,19 @@ func TestBatchReceiptFromRecordLabel(t *testing.T) {
 }
 
 // - look up a CD by its title 🚧
+func TestLookUpCDByTitle(t *testing.T) {
+	// Arrange
+	is := is.New(t)
+	whse := warehouse.New()
+	newCD := warehouse.NewCD("S&M", "Vertigo Records", 14.99, 42)
+	_, err := whse.Add(newCD)
+	is.NoErr(err) // Could not add CD to warehouse
+
+	// Act
+	result, err := whse.FindCDByTitle("S&M")
+
+	// Assert
+	is.NoErr(err)
+	is.Equal(result.GetTitle(), "S&M") // Title of result does not match
+	is.Equal(result.GetPrice(), 14.99) // Price of result does not match
+}
